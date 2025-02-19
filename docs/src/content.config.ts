c84d7ccaf7751ae0239dc465435f9bf1725d6802
ngnijland <ngnijland@gmail.com>
2025-02-19 18:14:01 +0100

    fix: formatter

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -3,13 +3,14 @@ import { z, defineCollection } from "astro:content";
 
 const feature = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: "./src/collections/feature" }),
-  schema: ({ image }) => z.object({
+  schema: ({ image }) =>
+    z.object({
       title: z.string(),
       image: image(),
       imageAlt: z.string(),
-      imageAlignment: z.enum(['left', 'right']),
+      imageAlignment: z.enum(["left", "right"]),
       imageColumnSpan: z.union([z.literal(6), z.literal(8)]),
-      sortOrder: z.number()
+      sortOrder: z.number(),
     }),
 });
 
