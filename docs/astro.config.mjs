2d6f38d3d1a6034bd8a8ec683022acb5e59c94fe
Antoine van der Lee <4329185+AvdLee@users.noreply.github.com>
2025-04-03 14:31:53 +0200

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://rocketsim.app",
-  base: "/RocketSimApp/",
   /**
    * Add the sitemap plugin
    *
