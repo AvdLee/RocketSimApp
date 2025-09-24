9cddc9fb873103dc62206c3f26fbf2dbc9af75c1
ngnijland <ngnijland@gmail.com>
2025-09-24 18:09:16 +0200

    feat: add featured image and fix semantics

--- docs/src/lib/utils/dateFormat.ts
+++ docs/src/lib/utils/dateFormat.ts
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 
 export const dateFormat = (
   date: Date | string,
-  pattern: string = "dd MMM, yyyy"
+  pattern: string = "dd MMM, yyyy",
 ): string => {
   const dateObj = new Date(date);
   const output = format(dateObj, pattern);
