ed124869e5e9924df8df34a5c4101ee801c64411
ngnijland <ngnijland@gmail.com>
2025-10-29 17:13:26 +0100

    feat: add more detailed footer

---
+++ docs/src/layouts/helpers/DynamicIcon.tsx
@@ -0,0 +1,33 @@
+import React, { type FC } from "react";
+import type { IconType } from "react-icons";
+import * as FaIcons from "react-icons/fa6";
+
+type IconMap = Record<string, IconType>;
+
+interface IDynamicIcon extends React.SVGProps<SVGSVGElement> {
+  icon: string;
+  className?: string;
+}
+
+const iconLibraries: { [key: string]: IconMap } = {
+  fa: FaIcons,
+};
+
+const DynamicIcon: FC<IDynamicIcon> = ({ icon, ...props }) => {
+  const IconLibrary = getIconLibrary(icon);
+  const Icon = IconLibrary ? IconLibrary[icon] : undefined;
+
+  if (!Icon) {
+    return <span className="text-sm">Icon not found</span>;
+  }
+
+  return <Icon {...props} />;
+};
+
+const getIconLibrary = (icon: string): IconMap | undefined => {
+  const libraryKey = icon.substring(0, 2).toLowerCase();
+
+  return iconLibraries[libraryKey];
+};
+
+export default DynamicIcon;
