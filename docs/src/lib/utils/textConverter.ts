727e31f5fec504cb45414ae3c7ab54f861fc617f
ngnijland <ngnijland@gmail.com>
2025-12-31 10:32:15 +0100

    fix: remove unused functions

--- docs/src/lib/utils/textConverter.ts
+++ docs/src/lib/utils/textConverter.ts
@@ -15,31 +15,3 @@ export const humanize = (content: string) => {
       return m.toUpperCase();
     });
 };
-
-// plainify
-export const plainify = (content: string) => {
-  const parseMarkdown: any = marked.parse(content);
-  const filterBrackets = parseMarkdown.replace(/<\/?[^>]+(>|$)/gm, "");
-  const filterSpaces = filterBrackets.replace(/[\r\n]\s*[\r\n]/gm, "");
-  const stripHTML = htmlEntityDecoder(filterSpaces);
-  return stripHTML;
-};
-
-// strip entities for plainify
-const htmlEntityDecoder = (htmlWithEntities: string) => {
-  let entityList: { [key: string]: string } = {
-    "&nbsp;": " ",
-    "&lt;": "<",
-    "&gt;": ">",
-    "&amp;": "&",
-    "&quot;": '"',
-    "&#39;": "'",
-  };
-  let htmlWithoutEntities: string = htmlWithEntities.replace(
-    /(&amp;|&lt;|&gt;|&quot;|&#39;)/g,
-    (entity: string): string => {
-      return entityList[entity];
-    },
-  );
-  return htmlWithoutEntities;
-};
